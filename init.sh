3b5e69adfdcccc76eec5b0a211befd7e67ef4d78
Adrian Imboden <mail@adrianimboden.ch>
2015-03-16 11:52:19 +0100

    fixed init.sh

--- init.sh
+++ init.sh
@@ -2,9 +2,7 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
 cd robo
-rm ProcessorExpert.pe || true
 cp ProcessorExpert.pe.template ProcessorExpert.pe
 cd ..
 cd remotecontrol
-rm ProcessorExpert.pe || true
 cp ProcessorExpert.pe.template ProcessorExpert.pe
