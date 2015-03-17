f72dae743677cb6591db373e817e6b7d7eb5696d
Adrian Imboden <mail@adrianimboden.ch>
2015-03-17 09:57:47 +0100

    build.sh generates now doxygen documentation

--- build.sh
+++ build.sh
@@ -16,4 +16,9 @@ echo -------------- generate processorexpert
 kinetis-design-studio -noSplash -data $TMPDIR/workspace -application com.freescale.processorexpert.core.PExApplication -generateAll
 echo -------------- build project
 kinetis-design-studio -noSplash -data $TMPDIR/workspace -application org.eclipse.cdt.managedbuilder.core.headlessbuild -build all
-
+echo -------------- generate documentation
+cd remotecontrol/doxy
+doxygen .
+cd ../..
+cd remotecontrol/doxy
+doxygen .
