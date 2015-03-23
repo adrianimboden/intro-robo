e7441efd440b9d771489a12cd4cbb784dfcbf227
Christian Di Battista <christian.dibattista@hslu.ch>
2015-03-23 08:45:35 +0100

    added Buzzer and Trigger

---
+++ robocommon/Buzzer.h
@@ -0,0 +1,60 @@
+/**
+ * \file
+ * \brief Buzzer driver interface.
+ * \author Erich Styger, erich.styger@hslu.ch
+ *
+ * This is the interface to the buzzer.
+ */
+
+#ifndef BUZZER_H_
+#define BUZZER_H_
+
+#if PL_HAS_SHELL
+  #include "CLS1.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*!
+ * \brief Shell parser routine.
+ * \param cmd Pointer to command line string.
+ * \param handled Pointer to status if command has been handled. Set to TRUE if command was understood.
+ * \param io Pointer to stdio handle
+ * \return Error code, ERR_OK if everything was ok.
+ */
+  uint8_t BUZ_ParseCommand(const unsigned char *cmd, bool *handled, const CLS1_StdIOType *io);
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif
+
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*!
+ * \brief Let the buzzer sound for a specified time.
+ * \param freqHz Frequency of the sound. Ignored if the buzzer is not supporting it.
+ * \param durationMs Duration in milliseconds.
+ * \return Error code, ERR_OK if everything is fine.
+ */
+uint8_t BUZ_Beep(uint16_t freqHz, uint16_t durationMs);
+
+/*!
+ * \brief Initialization of the driver
+ */
+void BUZ_Init(void);
+
+/*!
+ * \brief De-initialization of the driver
+ */
+void BUZ_Deinit(void);
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif
+
+#endif /* BUZZER_H_ */
