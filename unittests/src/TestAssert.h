e59babcc9001173f5d814f00a3da54a52490b56b
Adrian Imboden <mail@adrianimboden.ch>
2015-03-24 10:51:46 +0100

    improved assertion error output for tests

--- unittests/src/TestAssert.h
+++ unittests/src/TestAssert.h
@@ -1,12 +1,30 @@
 #ifndef TESTASSERT_H
 #define TESTASSERT_H
 
+#include <exception>
+
+class AssertionFailedException : public std::exception
+{
+public:
+	AssertionFailedException() noexcept
+	{
+	}
+
+	~AssertionFailedException() noexcept
+	{
+	}
+
+	const char* what() const noexcept override
+	{
+		return "assertion failed";
+	}
+};
 
 inline void testAssert(bool ok)
 {
 	if (!ok)
 	{
-		abort();
+		throw AssertionFailedException();
 	}
 }
 
