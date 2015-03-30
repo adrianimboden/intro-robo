bbdedb1cdc03b8e53535435ae8c475466b8e235d
Adrian Imboden <mail@adrianimboden.ch>
2015-03-30 11:42:47 +0200

    fixed warning

--- robocommon/CommandParser.h
+++ robocommon/CommandParser.h
@@ -86,7 +86,7 @@ namespace detail
 
 			auto currParam = 0u;
 
-			decltype(cmdToExecute.begin()) paramStart;
+			decltype(cmdToExecute.begin()) paramStart = cmdToExecute.begin();
 			char usedQuotationCharacter = '\0';
 
 			const auto cmdSize = cmdToExecute.size();
